a4ee1da26d5f6e47ae3a1fb365c08245c8a70e8c
Sam Hakala <samthakala@gmail.com>
2018-01-18 16:42:01 -0700

    Added automatic reset functionality to the add friends button.

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,14 +1,14 @@
 angular.module("friendsList").controller("mainCtrl", function($scope){
 
-$scope.name = "Sam"
+  $scope.name = "Sam"
 
-$scope.friends = ['mo', 'jess', 'josh', 'jeff', 'emma']
+  $scope.friends = ['mo', 'jess', 'josh', 'jeff', 'emma']
 
-$scope.newFriend
+  $scope.newFriend
 
-$scope.addFriend = function(){
-  console.log($scope.friends)
-  $scope.friends.push($scope.newFriend);
-}
+  $scope.addFriend = function(){
+    $scope.friends.push($scope.newFriend);
+    $scope.newFriend = ""
+  }
 
 });
